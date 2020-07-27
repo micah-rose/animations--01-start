e3ddaa11069d4bb6b802683cb62c2fe16bfd27d3
Micah Lund <micah.r.lund@gmail.com>
2020-07-27 14:06:29 -0600

    Wrote examples of using react transition group.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import Transition from 'react-transition-group/Transition';
 import "./App.css";
 import Modal from "./components/Modal/Modal";
 import Backdrop from "./components/Backdrop/Backdrop";
@@ -29,17 +29,27 @@ class App extends Component {
           onClick={() => 
             this.setState(prevState => ({showBlock: !prevState.showBlock}))}>
           Toggle</button>
-          <br></br>
-        {this.state.showBlock ? (
-        <div 
-          style={{
-            backgroundColor: 'red', 
-            width: 100, 
-            height: 100,
-            margin: 'auto'
-            }}
-        />
-        ) : null}
+          <br />
+        {/* {this.state.showBlock ? ( */}
+        <Transition 
+          in={this.state.showBlock} 
+          timeout={1000}
+          mountOnEnter
+          unmountOnExit>
+        {state => (
+                  <div 
+                  style={{
+                    backgroundColor: 'red', 
+                    width: 100, 
+                    height: 100,
+                    margin: 'auto',
+                    transition: 'opacity 1s ease-out',
+                    opacity: state === 'exiting' ? 0 : 1
+                    }}
+                />
+        )}
+        </Transition>
+        {/* ) : null} */}
         {this.state.modalIsOpen ? <Modal show={this.state.modalIsOpen} closed={this.closeModal}/> : null}
         {this.state.modalIsOpen ? <Backdrop show={this.state.modalIsOpen}/> : null }
         <button className="Button" onClick={this.showModal}>Open Modal</button>
