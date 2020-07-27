f6846a7c708f8a5f2b1d73a66d96327afe6668dd
Micah Lund <micah.r.lund@gmail.com>
2020-07-27 13:34:10 -0600

    Set up a new button to toggle a red square onto the screen in preparation for using the newly installed react transition group package.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ import List from "./components/List/List";
 class App extends Component {
 
   state = {
-    modalIsOpen: false
+    modalIsOpen: false,
+    showBlock: false
   }
 
   showModal = () => {
@@ -23,8 +24,24 @@ class App extends Component {
     return (
       <div className="App">
         <h1>React Animations</h1>
-        <Modal show={this.state.modalIsOpen} closed={this.closeModal}/>
-        <Backdrop show={this.state.modalIsOpen}/>
+        <button 
+          className="Button"
+          onClick={() => 
+            this.setState(prevState => ({showBlock: !prevState.showBlock}))}>
+          Toggle</button>
+          <br></br>
+        {this.state.showBlock ? (
+        <div 
+          style={{
+            backgroundColor: 'red', 
+            width: 100, 
+            height: 100,
+            margin: 'auto'
+            }}
+        />
+        ) : null}
+        {this.state.modalIsOpen ? <Modal show={this.state.modalIsOpen} closed={this.closeModal}/> : null}
+        {this.state.modalIsOpen ? <Backdrop show={this.state.modalIsOpen}/> : null }
         <button className="Button" onClick={this.showModal}>Open Modal</button>
         <h3>Animating Lists</h3>
         <List />
